4dd5158e4e25fa55dfda9721b67bb2057f2252d4
truongvu885 <truongvu885@gmai.com>
2022-04-25 13:56:18 +0700

    dịch vụ

--- src/DichVu.java
+++ src/DichVu.java
@@ -4,4 +4,16 @@ public class DichVu {
     private int maLoaiDV;
     private String tenDV;
     private float giaDV;
+
+    public int getMaLoaiDV() {
+        return maLoaiDV;
+    }
+
+    public String getTenDV() {
+        return tenDV;
+    }
+
+    public float getGiaDV() {
+        return giaDV;
+    }
 }
