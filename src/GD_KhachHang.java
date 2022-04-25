15c52f809ae95b6225713c3243fb19cc8bf94fe8
truongvu885 <truongvu885@gmail.com>
2022-04-25 14:34:35 +0700

    hello

---
+++ src/GD_KhachHang.java
@@ -0,0 +1,22 @@
+package src;
+
+import javax.swing.*;
+
+public class GD_KhachHang {
+    private JList list1;
+    private JTextField txt_maKH;
+    private JTextField txt_tenKH;
+    private JTextField txt_diachi;
+    private JTextField txt_ngaysinh;
+    private JTextField textField1;
+    private JRadioButton nữRadioButton;
+    private JTextField txt_cccd;
+    private JComboBox cb_thanhvien;
+    private JTextField txt_ngaythuegannhat;
+    private JRadioButton nam;
+    private JPanel nu;
+    private JButton btn_them;
+    private JButton btn_sua;
+    private JButton btn_xoa;
+    private JButton btn_sapxep;
+}
